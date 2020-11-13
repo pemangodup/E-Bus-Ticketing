8650869f5ebacff0b46076f697a42c7fa045ebd7
pemangodup <pngodup123@gmail.com>
2020-11-13 13:33:57 +0545

    clear warnings

--- lib/view/sign_up.dart
+++ lib/view/sign_up.dart
@@ -1,8 +1,6 @@
-import 'package:ebusticketing/view/add_bus_detail.dart';
 import 'package:ebusticketing/view/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 
 
@@ -18,7 +16,6 @@ class _SignUpState extends State<SignUp> {
 
 
   final FirebaseAuth auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
